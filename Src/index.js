e81580a4f718a71b7b7842091e3001b4e0ef3ad9
MbhekeniN <159086527+MbhekeniN@users.noreply.github.com>
2024-07-28 23:18:17 +0200

    Injected more weather elements.
    Injected data for:
    Conditions
    Humidity
    Wind speed
    Icon

--- Src/index.js
+++ Src/index.js
@@ -4,12 +4,20 @@ function displayTemperature (response) {
     temperatureElement.innerHTML = Temperature;
     let h1 = document.querySelector("#current-city")
     h1.innerHTML = response.data.city;
+    let conditionsElement = document.querySelector("#current-Conditions");
+    let humidityElement = document.querySelector("#current-Humidity");
+    let windSpeedElement = document.querySelector("#current-Wind");
+    let iconElement = document.querySelector("#icon")
+    conditionsElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windSpeedElement.innerHTML = response.data.wind.speed;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="Icon" />`;
 }
 
 function search (event) {
     event.preventDefault();
     let input = document.querySelector ("#search-Input");
-    let city = input.value ;
+    let city = input.value;
 
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=2fo4a0349e06dtd35a3306b4b26aea49`;
 
